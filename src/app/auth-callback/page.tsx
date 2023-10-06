acc160f3a04cd8ce35f5d185c9d13a76c64c858e
Dawid Derewońko <angeldeabyssi@gmail.com>
2023-10-07 01:40:41 +0200

    trpc implementation

---
+++ src/app/auth-callback/page.tsx
@@ -0,0 +1,21 @@
+import {useRouter, useSearchParams} from "next/navigation";
+import {trpc} from "@/app/_trpc/client";
+
+const Page = async () => {
+    const router = useRouter();
+
+    const searchParams = useSearchParams();
+    const origin = searchParams.get('origin');
+
+
+
+    const {data, isLoading} = trpc.authCallback.useQuery(undefined, {
+        onSuccess: ({success}) => {
+            if (success) {
+                router.push(origin ? `/${origin}` : '/dashboard')
+            }
+        }
+    })
+}
+
+export default Page;
