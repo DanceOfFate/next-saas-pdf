8e1d3f73008844b9b43f22f53509b1609b9f3728
Dawid Derewońko <angeldeabyssi@gmail.com>
2023-10-06 21:46:30 +0200

    landing page, a lot of styling

--- src/components/MaxWidthWrapper.tsx
+++ src/components/MaxWidthWrapper.tsx
@@ -9,7 +9,7 @@ const MaxWidthWrapper = ({
     children: ReactNode
 }) => {
     return(
-        <div className={cn('mx-auto w-full max-w-screen-xl md:px-2.5', className)}>
+        <div className={cn('mx-auto w-full max-w-screen-xl md:px2.5 items-center text-center', className)}>
             {children}
         </div>
     )
